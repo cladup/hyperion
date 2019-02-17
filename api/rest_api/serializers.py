3e26d32f7ca52f89eb0df8afdfbfa1d472dafbd8
StolenByte <thscndgh@gmail.com>
2019-02-17 20:28:54 +0900

    updated

--- api/rest_api/serializers.py
+++ api/rest_api/serializers.py
@@ -3,15 +3,16 @@
 # coded by StolenByte
 __AUTHOR__ = 'StolenByte [thscndgh@gmail.com]'
 
+from django.db.models import Q
 from rest_framework import routers, serializers, viewsets
+
 from .models import campaigns as Campaigns, display_stands as Display_Stands, products as Products
-from collections import OrderedDict
 
 class products_serializer(serializers.ModelSerializer):
 	class Meta:
 		model = Products
 		fields = (
-			'name', 'type',
+			'id', 'name', 'type',
 			'position_x', 'position_y', 'position_z',
 			'rotation_x', 'rotation_y', 'rotation_z',
 			'scale', 'format', 'click_event', 'animation'
@@ -23,7 +24,7 @@ class display_stands_serializer(serializers.ModelSerializer):
 	class Meta:
 		model = Display_Stands
 		fields = (
-			'name', 'type',
+			'id', 'name', 'type',
 			'position_x', 'position_y', 'position_z',
 			'rotation_x', 'rotation_y', 'rotation_z',
 			'scale', 'format', 'click_event', 'animation',
@@ -36,52 +37,87 @@ class campaigns_serializer(serializers.ModelSerializer):
 	class Meta:
 		model = Campaigns
 		fields = (
-			'company', 'title',
+			'id', 'company', 'title',
 			'position_x', 'position_y', 'position_z',
 			'rotation_x', 'rotation_y', 'rotation_z',
 			'display_stands'
 		)
 
 	def create(self, validated_data):
-		dss = validated_data.pop('display_stands')
-		
-		c = Campaigns.objects.update_or_create(**validated_data)
+		# dss = validated_data.pop('display_stands')
+		c_id = self.initial_data.get('id')
+		dss = self.initial_data.get('display_stands')
+
+		if c_id:
+			c = Campaigns.objects.filter(Q(id=c_id))
+			if c.exists():
+				c = c.first()
+			else:
+				return None
+		else:
+			c = Campaigns.objects.create()
+
+		c.company = self.initial_data.get('company')
+		c.title = self.initial_data.get('title')
+		c.position_x = self.initial_data.get('position_x')
+		c.position_y = self.initial_data.get('position_y')
+		c.position_z = self.initial_data.get('position_z')
+		c.rotation_x = self.initial_data.get('rotation_x')
+		c.rotation_y = self.initial_data.get('rotation_y')
+		c.rotation_z = self.initial_data.get('rotation_z')
+
 		for ds in dss:
-			d = Display_Stands.objects.update_or_create(
-				name=ds['name'],
-				type=ds['type'],
-				position_x=ds['position_x'],
-				position_y=ds['position_y'],
-				position_z=ds['position_z'],
-				rotation_x=ds['rotation_x'],
-				rotation_y=ds['rotation_y'],
-				rotation_z=ds['rotation_z'],
-				scale=ds['scale'],
-				format=ds['format'],
-				click_event=ds['click_event'],
-				animation=ds['animation'],
-			)
+			d = None
+			if ('id' in ds):
+				d = c.display_stands.filter(Q(id=ds['id']))
+				if d.exists():
+					d = d.first()
+				else:
+					continue
+			else:
+				d = Display_Stands.objects.create()
+			
+			d.name = ds['name']
+			d.type = ds['type']
+			d.position_x = ds['position_x']
+			d.position_y = ds['position_y']
+			d.position_z = ds['position_z']
+			d.rotation_x = ds['rotation_x']
+			d.rotation_y = ds['rotation_y']
+			d.rotation_z = ds['rotation_z']
+			d.scale = ds['scale']
+			d.format = ds['format']
+			d.click_event = ds['click_event']
+			d.animation = ds['animation']
 
 			for ps in ds['products']:
-				p = Products.objects.update_or_create(
-					name=ds['name'],
-					type=ds['type'],
-					position_x=ds['position_x'],
-					position_y=ds['position_y'],
-					position_z=ds['position_z'],
-					rotation_x=ds['rotation_x'],
-					rotation_y=ds['rotation_y'],
-					rotation_z=ds['rotation_z'],
-					scale=ds['scale'],
-					format=ds['format'],
-					click_event=ds['click_event'],
-					animation=ds['animation'],
-				)
+				p = None
+				if ('id' in ps):
+					p = d.products.filter(Q(id=ps['id']))
+					if p.exists():
+						p = p.first()
+					else:
+						continue
+				else:
+					p = Products.objects.create()
+
+				p.name = ps['name']
+				p.type = ps['type']
+				p.position_x = ps['position_x']
+				p.position_y = ps['position_y']
+				p.position_z = ps['position_z']
+				p.rotation_x = ps['rotation_x']
+				p.rotation_y = ps['rotation_y']
+				p.rotation_z = ps['rotation_z']
+				p.scale = ps['scale']
+				p.format = ps['format']
+				p.click_event = ps['click_event']
+				p.animation = ps['animation']
 				p.save()
+
 				d.products.add(p)
 
 			d.save()
-			c.display_stands.add(d)
 
 		c.display_stands.add(d)
 		c.save()
--- readme.md
+++ readme.md
@@ -1,7 +1,89 @@
 # API Reference
 
-
 ## GET /api/campaign/get/[campaign number]
 
-* Description
-  * Get Campaign Data
+Get Camapaign Data
+> **Note:** If the campaign number does not  exist, output all data
+
+## PUT /api/campaign/set/
+
+Set or Update Camapaign Data
+> **Note:** In case of update, the corresponding "id" value must exist before the data can be updated
+> 
+PoC
+```
+Content-Type: application/json
+Test Parameter
+{
+    "company":"Campaigns 2",
+    "title":"Campaigns 2",
+    "position_x":"1.23",
+    "position_y":"1.23",
+    "position_z":"1.23",
+    "rotation_x":"2.34",
+    "rotation_y":"2.34",
+    "rotation_z":"2.34",
+    "display_stands":[
+        {
+            "name":"Display 2",
+            "type":"Display 2",
+            "position_x":"1.23",
+            "position_y":"1.23",
+            "position_z":"1.23",
+            "rotation_x":"2.34",
+            "rotation_y":"2.34",
+            "rotation_z":"2.34",
+            "scale":"2.34",
+            "format":"a",
+            "click_event":"b",
+            "animation":"c",
+            "products":[
+                {
+                    "name":"Product 2",
+                    "type":"Product 2",
+                    "position_x":"1.23",
+                    "position_y":"1.23",
+                    "position_z":"1.23",
+                    "rotation_x":"2.34",
+                    "rotation_y":"2.34",
+                    "rotation_z":"2.34",
+                    "scale":"1.23",
+                    "format":"a",
+                    "click_event":"b",
+                    "animation":"c"
+                }
+            ]
+        },
+        {
+            "name":"Display 2",
+            "type":"Display 2",
+            "position_x":"1.23",
+            "position_y":"1.23",
+            "position_z":"1.23",
+            "rotation_x":"2.34",
+            "rotation_y":"2.34",
+            "rotation_z":"2.34",
+            "scale":"2.34",
+            "format":"a",
+            "click_event":"b",
+            "animation":"c",
+            "products":[
+                {
+                    "name":"Product 2",
+                    "type":"Product 2",
+                    "position_x":"1.23",
+                    "position_y":"1.23",
+                    "position_z":"1.23",
+                    "rotation_x":"2.34",
+                    "rotation_y":"2.34",
+                    "rotation_z":"2.34",
+                    "scale":"1.23",
+                    "format":"a",
+                    "click_event":"b",
+                    "animation":"c"
+                }
+            ]
+        }
+    ]
+
+```
